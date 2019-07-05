f090f2814ca4594a24e4acca6fe7fb45518971bd
Ente <ducksource@duckpond.ch>
2019-07-05 16:01:09 +0200

    Delete hidden pull request refs
    Github creates hidden pull request refs.
    But when pushing to github those refs are rejected,
    causing the git to terminate with an error. Therefore
    we have to always delete those refs before pushing.

--- run.sh
+++ run.sh
@@ -11,16 +11,28 @@ HTTP_DST_PROXY="${HTTP_DST_PROXY:-""}"
 
 SLEEP_TIME="${SLEEP_TIME:-60s}"
 
+DELETE_REFS_PATTERN="${DELETE_REFS_PATTERN:-refs/pull}"
+
 git config --global "http.sslVerify" "${HTTP_TLS_VERIFY}"
 git config --global "http.${SRC_REPO}.proxy" "${HTTP_SRC_PROXY}"
 git config --global "http.${DST_REPO}.proxy" "${HTTP_DST_PROXY}"
 
 LOCAL_REPO="$(mktemp -d)"
-git clone --mirror "${SRC_REPO}" "${LOCAL_REPO}"
+
+git clone \
+  --mirror \
+  "${SRC_REPO}" "${LOCAL_REPO}"
 
 cd "${LOCAL_REPO}"
 while true; do
   git remote update
+
+  # delete all hidden github pull request refs
+  git for-each-ref \
+    --format='delete %(refname)' \
+    "${DELETE_REFS_PATTERN}" \
+  | git update-ref --stdin
+
   git push --mirror "${DST_REPO}"
   sleep "${SLEEP_TIME}"
 done
